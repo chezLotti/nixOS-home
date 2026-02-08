e3efed8369890554c436f8ce96ee5f1a4b4a2973
chezLotti <charlottelhaw@gmail.com>
2026-02-08 20:17:08 +0100

    cleanup user config

--- home/charlotte.nix
+++ home/charlotte.nix
@@ -1,13 +1,15 @@
 { config, lib, pkgs, systemSettings, home-manager, ... }:
 
-
+let 
+  username = "charlotte";
+in
 {
   imports = [];
 
 
-	users.users."charlotte" = {
+	users.users.${username} = {
     isNormalUser = true;
-    description = "Charlotte";
+    description = username;
     shell = lib.mkForce pkgs.fish;
     extraGroups = [ "networkmanager" "wheel" "input" ];
     packages = with pkgs; [
@@ -19,9 +21,9 @@
 
   home-manager.backupFileExtension = "backup";
 
-  home-manager.users."charlotte" = {
-	  home.username = "charlotte";
-	  home.homeDirectory = "/home/charlotte";
+  home-manager.users.${username} = {
+	  home.username = username;
+	  home.homeDirectory = "/home/"+username;
 
 	  programs.git = {
 	    enable = true;
@@ -51,67 +53,12 @@
       };
     };
 
-	  # This value determines the Home Manager release that your configuration is
-	  # compatible with. This helps avoid breakage when a new Home Manager release
-	  # introduces backwards incompatible changes.
-	  #
-	  # You should not change this value, even if you update Home Manager. If you do
-	  # want to update the value, then make sure to first check the Home Manager
-	  # release notes.
-	  home.stateVersion = systemSettings.stateVersion; # Please read the comment before changing.
+	  home.stateVersion = systemSettings.stateVersion;
 
-	  # The home.packages option allows you to install Nix packages into your
-	  # environment.
-	  home.packages = with pkgs; [
-	    # # Adds the 'hello' command to your environment. It prints a friendly
-	    # # "Hello, world!" when run.
-	    # pkgs.hello
+	  home.packages = with pkgs; [];
 
-	    # # It is sometimes useful to fine-tune packages, for example, by applying
-	    # # overrides. You can do that directly here, just don't forget the
-	    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-	    # # fonts?
-	    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+	  home.file = {};
 
-	    # # You can also create simple shell scripts directly inside your
-	    # # configuration. For example, this adds a command 'my-hello' to your
-	    # # environment:
-	    # (pkgs.writeShellScriptBin "my-hello" ''
-	    #   echo "Hello, ${config.home.username}!"
-	    # '')
-	  ];
-
-	  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-	  # plain files is through 'home.file'.
-	  home.file = {
-	    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-	    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-	    # # symlink to the Nix store copy.
-	    # ".screenrc".source = dotfiles/screenrc;
-
-	    # # You can also set the file content immediately.
-	    # ".gradle/gradle.properties".text = ''
-	    #   org.gradle.console=verbose
-	    #   org.gradle.daemon.idletimeout=3600000
-	    # '';
-	  };
-
-	  # Home Manager can also manage your environment variables through
-	  # 'home.sessionVariables'. These will be explicitly sourced when using a
-	  # shell provided by Home Manager. If you don't want to manage your shell
-	  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-	  # located at either
-	  #
-	  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-	  #
-	  # or
-	  #
-	  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-	  #
-	  # or
-	  #
-	  #  /etc/profiles/per-user/charlotte/etc/profile.d/hm-session-vars.sh
-	  #
 	  home.sessionVariables = {
 	    # EDITOR = "emacs";
 	  };
